d3e8347c7d6a1ada6e3af6f8f904d7df82e5febe
Guilherme Pereira Schneidt <guilhermepsch@gmail.com>
2022-04-23 22:46:06 -0300

    Realizado exercícios do BlueJ

--- 2SM/25PRO1/bluej/Shapes/DesenharObraDeArte.java
+++ 2SM/25PRO1/bluej/Shapes/DesenharObraDeArte.java
@@ -7,6 +7,38 @@ public class DesenharObraDeArte
         Quadrado janela = new Quadrado();
         Triangulo grama = new Triangulo();
         Triangulo telhado = new Triangulo();
+
+        sol.cor = "yellow";
+	sol.diametro = 50;
+	sol.x = 240;
+	sol.y = 20;
+	
+	parede.cor = "red";
+	parede.lado = 120;
+	parede.x = 90;
+	parede.y = 100;
         
+	janela.cor = "black";
+	janela.lado = 40;
+	janela.x = 110;
+	janela.y = 120;
+	
+	grama.base = 300;
+        grama.cor = "green";
+        grama.altura = 150;
+        grama.x = 150;
+        grama.y = 100;
+
+	telhado.cor = "blue";
+	telhado.altura = 50;
+	telhado.base = 150;
+	telhado.x = 148;
+	telhado.y = 50;
+        
+        grama.desenhar();
+        parede.desenhar();
+        janela.desenhar();
+        telhado.desenhar();
+        sol.desenhar();
     }
 }
